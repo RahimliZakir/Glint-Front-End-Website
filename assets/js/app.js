eac115e8888d2ed5a32a783a3a868e2a3bdf484c
RahimliZakir <zakirer@code.edu.az>
2021-10-08 01:31:31 +0400

    Some Aside Improvements

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,10 @@
 $(function () {
     var scrollTop = $(".scroll-top");
+    var sidebarBtn = $(".sidebar-btn");
+    var aside = $("aside");
+    var asideRow = $(".aside-row");
+    var asideShadow = $(".ar-shadow");
+    var asideCrossBtn = $(".aside-cross-btn");
     $(window).on("scroll", function () {
         var scroll = $(window).scrollTop();
         if (scroll > 125) {
@@ -8,23 +13,32 @@ $(function () {
         else {
             $(scrollTop).removeClass("scroll-vis");
         }
+        if (scroll > 25) {
+            $(sidebarBtn).addClass("sidebar-scroll");
+        }
+        else {
+            $(sidebarBtn).removeClass("sidebar-scroll");
+        }
     });
-    var sidebarBtn = $(".sidebar-btn");
-    var aside = $("aside");
-    var asideCrossBtn = $(".aside-cross-btn");
     $(aside).click(function (e) {
         e.stopPropagation();
     });
     $(sidebarBtn).on("click", function (e) {
         e.stopPropagation();
         $(aside).addClass("show");
+        $(asideRow).addClass("ar-show");
+        $(asideShadow).addClass("ar-shadow-vis");
     });
     $(asideCrossBtn).on("click", function () {
         $(aside).removeClass("show");
+        $(asideRow).removeClass("ar-show");
+        $(asideShadow).removeClass("ar-shadow-vis");
     });
     $(document.body).click(function (e) {
         if ($(aside).hasClass("show")) {
             $(aside).removeClass("show");
+            $(asideRow).removeClass("ar-show");
+            $(asideShadow).removeClass("ar-shadow-vis");
         }
     });
 });
--- assets/ts/app.ts
+++ assets/ts/app.ts
@@ -1,6 +1,12 @@
 $(function () {
   let scrollTop: any = $(".scroll-top");
 
+  let sidebarBtn: any = $(".sidebar-btn");
+  let aside: any = $("aside");
+  let asideRow: any = $(".aside-row");
+  let asideShadow: any = $(".ar-shadow");
+  let asideCrossBtn: any = $(".aside-cross-btn");
+
   $(window).on("scroll", () => {
     let scroll: any = $(window).scrollTop();
 
@@ -9,11 +15,13 @@ $(function () {
     } else {
       $(scrollTop).removeClass("scroll-vis");
     }
-  });
 
-  let sidebarBtn: any = $(".sidebar-btn");
-  let aside: any = $("aside");
-  let asideCrossBtn: any = $(".aside-cross-btn");
+    if (scroll > 25) {
+      $(sidebarBtn).addClass("sidebar-scroll");
+    } else {
+      $(sidebarBtn).removeClass("sidebar-scroll");
+    }
+  });
 
   $(aside).click((e) => {
     e.stopPropagation();
@@ -22,15 +30,21 @@ $(function () {
   $(sidebarBtn).on("click", function (e) {
     e.stopPropagation();
     $(aside).addClass("show");
+    $(asideRow).addClass("ar-show");
+    $(asideShadow).addClass("ar-shadow-vis");
   });
 
   $(asideCrossBtn).on("click", function () {
     $(aside).removeClass("show");
+    $(asideRow).removeClass("ar-show");
+    $(asideShadow).removeClass("ar-shadow-vis");
   });
 
   $(document.body).click(function (e) {
     if ($(aside).hasClass("show")) {
       $(aside).removeClass("show");
+      $(asideRow).removeClass("ar-show");
+      $(asideShadow).removeClass("ar-shadow-vis");
     }
   });
 });
